1df0d14550ba88ce75e1941c1179ebb13feacafe
Jess Somaiya <jess_somaiya@hotmail.com>
2022-09-30 14:38:53 +0100

    Feat: Decode error messages from Looker API (#5)

--- go/go.mod
+++ go/go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/json-iterator/go v1.1.12
-	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5 // indirect
+	github.com/smartystreets/goconvey v1.7.2 // indirect
+	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5
 	gopkg.in/ini.v1 v1.61.0
 )
---
+++ go/sdk/v4/errors.go
@@ -0,0 +1,67 @@
+package v4
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/looker-open-source/sdk-codegen/go/rtl"
+)
+
+var (
+	// ErrNotFound occurs when a requested resource cannot be found.
+	ErrNotFound = errors.New("not found")
+)
+
+// Error ensures that the Error type complies to the error interface.
+func (e Error) Error() string {
+	return e.Message
+}
+
+// Error ensures that the ValidationError type complies to the error interface.
+func (e ValidationError) Error() string {
+	if e.Errors == nil {
+		return e.Message
+	}
+
+	var errSlice []string
+	for _, m := range *e.Errors {
+		if m.Field != nil && m.Message != nil {
+			errSlice = append(errSlice, fmt.Sprintf("'%s' (%s)", *m.Field, *m.Message))
+		}
+	}
+	return fmt.Sprintf("validation error on fields: %s", strings.Join(errSlice, ", "))
+}
+
+// deseraliseError decodes the error message depending on the error response status.
+func deserialiseError(err error) error {
+	re, ok := err.(rtl.ResponseError)
+	if !ok {
+		return err
+	}
+
+	switch re.StatusCode {
+	case http.StatusUnprocessableEntity:
+		e := new(ValidationError)
+		if unmarshalErr := json.Unmarshal(re.Body, e); unmarshalErr != nil {
+			break
+		}
+		return e
+	case http.StatusNotFound:
+		e := new(Error)
+		if unmarshalErr := json.Unmarshal(re.Body, e); unmarshalErr != nil {
+			break
+		}
+		return fmt.Errorf("%s: %w", e.Message, ErrNotFound)
+	default:
+		e := new(Error)
+		if unmarshalErr := json.Unmarshal(re.Body, e); unmarshalErr != nil {
+			break
+		}
+		return e
+	}
+
+	return fmt.Errorf("error unmarshalling body with status:%s, body:%s, error:%s", re.Status, re.Body, err.Error())
+}
